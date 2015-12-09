d01ce54a2d1234224c8d6ed7207898a88d314d2e
priya <easwarapriyas@gmail.com>
2015-12-09 11:02:30 +0530

    first commit

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Practice Sesions Of AZURE";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
